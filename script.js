34173ccc0918ec8ee3d74d0c55eaa3271b16f125
srinivas3888 <160864752+srinivas3888@users.noreply.github.com>
2024-10-24 17:34:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+// import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:3000/');
+
+const msgs = document.getElementById('msgs');
+const form = document.getElementById('form');
+
+socket.on('connect', (soc)=>{
+    const p = document.createElement('h3');
+    p.innerText = `Joined with id: ${soc.id}`;
+    msgs.appendChild(p);
+})
+
+socket.on('msg', (msg)=>{
+    const p = document.createElement('p');
+    p.innerText = msg;
+    msgs.appendChild(p);
+})
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const p = document.createElement('p');
+    p.innerText = form.value;
+    msgs.appendChild(p);
+    
+    socket.emit('msg', form.value);
+})
